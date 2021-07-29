9d4b38ea5a4ae2298b86df01664637043a031411
Olivier Binette <olivier.binette@gmail.com>
2021-07-29 17:13:18 -0400

    add original app structure

--- R/runApp.R
+++ R/runApp.R
@@ -1,7 +1,4 @@
 #' @export
 runApp <- function() {
-  i18n <- shiny.i18n::Translator$new(translation_json_path=paste0(system.file("www/translate.js", package="YoloHealthApp")))
-  i18n$set_translation_language('English')
-
-  shinyApp(ui(i18n), serverFactory(i18n))
+  shiny::shinyAppDir(system.file("apps", package = "YoloHealthApp"))
 }
